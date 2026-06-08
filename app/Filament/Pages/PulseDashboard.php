e90e4163a978b77960d1704218025280be6b5390
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-06-08 14:16:40 +0300

    feat: create PulseDashboard Filament page for system monitoring

--- app/Filament/Pages/PulseDashboard.php
+++ app/Filament/Pages/PulseDashboard.php
@@ -6,7 +6,7 @@ use Filament\Pages\Page;
 
 class PulseDashboard extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-cpu-chip';
+    protected static $navigationIcon = 'heroicon-o-cpu-chip';
 
     protected static string $view = 'filament.pages.pulse-dashboard';
 
